61da7bdc557c097633d4f800e26d0413ec4f4717
[오종석][기술개발본부][사원] <ojs6889@datau.co.kr>
2025-12-05 09:56:35 +0900

    CompileSdk 35 -> 36 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "co.kr.datau.cicdpractice"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "co.kr.datau.cicdpractice"
@@ -25,11 +25,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
